21085143dcf5f9fdb52c4115c508a69a11076d36
yoimhere <yo_imhere@163.com>
2015-01-27 16:05:05 +0800

    add alertView block

---
+++ UIView/UIAlertView+Block.h
@@ -0,0 +1,17 @@
+//
+//  UIAlertView+Block.h
+//  Test
+//
+//  Created by Nick on 15-1-27.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void (^UIAlertViewZJCompletionBlock)(UIAlertView *alertView, NSUInteger buttonIndex);
+
+@interface UIAlertView (Block)
+
+@property (nonatomic, copy) UIAlertViewZJCompletionBlock zjCompletitionBlock;
+
+@end
