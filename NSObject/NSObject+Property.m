efe3fb7c57ed77700c8f4c902d8bcba3dc8406c2
yoimhere <yo_imhere@163.com>
2015-01-27 17:04:38 +0800

    add some code

---
+++ NSObject/NSObject+Property.m
@@ -0,0 +1,65 @@
+//
+//  NSObject+Property.m
+//  XMhouse
+//
+//  Created by Nick on 14-12-26.
+//  Copyright (c) 2014年 xmhouse. All rights reserved.
+//
+
+#import "NSObject+Property.h"
+#import <objc/runtime.h>
+
+
+NSInteger const objectStringEncode = NSUTF8StringEncoding;
+
+@implementation NSObject (Property)
+
+-(NSArray *)zj_GetPropertyAllKeys
+{
+    unsigned int outCount, i;
+
+    objc_property_t *properties = class_copyPropertyList([self class], &outCount);
+    NSMutableArray *propertyNames = [[NSMutableArray alloc] initWithCapacity:outCount];
+    for (i = 0; i < outCount; i++)
+    {
+        objc_property_t property = properties[i];
+        NSString *propertyNameString = [[NSString alloc] initWithCString:property_getName(property) encoding:objectStringEncode];
+        
+        [propertyNames addObject:propertyNameString];
+    }
+    return propertyNames;
+}
+
+-(NSString *)zj_GetPropertyClassStringWithKey :(NSString *)key
+{
+    if (key == nil || [key isEqualToString:@""])
+    {
+        return nil;
+    }
+    
+    objc_property_t property =  class_getProperty([self class],[key cStringUsingEncoding:NSUTF8StringEncoding]);
+    if (property == (__bridge objc_property_t)([NSNull null])) {
+        return nil;
+    }
+    const char * attrC = property_getAttributes(property);
+    NSString *str = [[NSString alloc] initWithCString:attrC encoding:objectStringEncode];
+    if (str.length == 0)
+    {
+        return nil;
+    }
+    
+    NSRange range = [str rangeOfString:@","];
+    str = [str substringWithRange:NSMakeRange(0, range.location)];
+
+    NSRange isObjRang = [str rangeOfString:@"@\""];
+    if (isObjRang.location != NSNotFound)
+    {
+        NSUInteger location = isObjRang.location + isObjRang.length;
+       str = [str substringWithRange:NSMakeRange(location,(str.length - location) - 1)];
+    }else{
+        str = [str substringWithRange:NSMakeRange(1, str.length -1)];
+    }
+    return str;
+}
+
+@end
---
+++ baseClass/ZJBaseCell.h
@@ -0,0 +1,16 @@
+//
+//  LLBaseCell.h
+//  XMhouse
+//
+//  Created by Nick on 14-10-23.
+//  Copyright (c) 2014年 onebyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJBaseCell : UITableViewCell
+
++(instancetype)cellWithTableView:(UITableView *)tableView;
++(CGFloat)cellHeigth;
+
+@end
