9fb0f97175190aee4d902f3ebef847a25033b5ed
yoimhere <yo_imhere@163.com>
2015-01-27 17:15:07 +0800

    add some code

---
+++ UIView/UITableViewCell+ZJCell.h
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell+ZJCell.h
+//  Test
+//
+//  Created by Nick on 15-1-27.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableViewCell (ZJCell)
+
++(instancetype)zjCellWithTableView:(UITableView *)tableView;
++(instancetype)zjCellWithTableView:(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath;
+
+@end
---
+++ UIView/UITableViewCell+ZJCell.m
@@ -0,0 +1,35 @@
+//
+//  UITableViewCell+ZJCell.m
+//  Test
+//
+//  Created by Nick on 15-1-27.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import "UITableViewCell+ZJCell.h"
+
+@implementation UITableViewCell (ZJCell)
+
+
++(instancetype)zjCellWithTableView:(UITableView *)tableView;
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(self)];
+    if (cell == nil) {
+        cell  = [[self alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass(self)];
+    }
+    
+    return cell;
+}
+
++(instancetype)zjCellWithTableView :(UITableView *)tableView forIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass(self) forIndexPath:indexPath];
+    if (cell == nil) {
+        cell  = [[self alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass(self)];
+    }
+    
+    return cell;
+}
+
+
+@end
