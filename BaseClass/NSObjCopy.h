e74bb8dd15513fdfa6895a205b2b2ce9f8e73289
yoimhere <yo_imhere@163.com>
2015-01-28 17:05:15 +0800

    init

---
+++ BaseClass/NSObjCopy.h
@@ -0,0 +1,13 @@
+//
+//  NSObjCopy.h
+//  Test
+//
+//  Created by Nick on 15-1-28.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObjCopy : NSObject<NSCopying,NSMutableCopying>
+
+@end
---
+++ BaseClass/NSObjCopy.m
@@ -0,0 +1,29 @@
+//
+//  NSObjCopy.m
+//  Test
+//
+//  Created by Nick on 15-1-28.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import "NSObjCopy.h"
+#import "NSObject+ZJProperty.h"
+
+@implementation NSObjCopy
+
+
+-(id)copyWithZone:(NSZone *)zone
+{
+    NSObject *copyObj = [[self class] new];
+    //待扩展
+    for (NSString *key in [self zjGetPropertyAllKeys]) {
+        [copyObj setValue:[self valueForKey:key] forKey:key];
+    }
+    return copyObj;
+}
+
+-(id)mutableCopyWithZone:(NSZone *)zone
+{
+    return [self copyWithZone:zone];
+}
+@end
