043d5518143ea8b758511b2ad8cf4539f32287d0
yoimhere <yo_imhere@163.com>
2015-01-30 16:00:17 +0800

    UIBezierPath 字符处处扩展，根据返回文字的UIBezierPath

---
+++ Fun/UIBezierPath+ZJText.h
@@ -0,0 +1,15 @@
+//
+//  UIBezierPath+ZJText.h
+//  Path
+//
+//  Created by Nick on 15-1-30.
+//  Copyright (c) 2015年 onebyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBezierPath (ZJText)
+
++(UIBezierPath *)zjBezierPathWithText:(NSString *)text attributes:(NSDictionary *)attrs;
+
+@end
